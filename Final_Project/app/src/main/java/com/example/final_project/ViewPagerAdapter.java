16cdf68778b8b4bb77cb80acfb7272d2161e543b
thinh2507 <thinhkg1258@gmail.com>
2022-12-22 16:50:02 +0700

    Change in code

---
+++ Final_Project/app/src/main/java/com/example/final_project/ViewPagerAdapter.java
@@ -0,0 +1,42 @@
+package com.example.final_project;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+import com.example.final_project.fragment.CartFragment;
+import com.example.final_project.fragment.HomeFragment;
+import com.example.final_project.fragment.ProductFragment;
+import com.example.final_project.fragment.ProfileFragment;
+import com.example.final_project.fragment.VoucherFragment;
+
+public class ViewPagerAdapter extends FragmentStatePagerAdapter {
+    public ViewPagerAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                return new HomeFragment();
+            case 1:
+                return new ProductFragment();
+            case 2:
+                return new CartFragment();
+            case 3:
+                return new VoucherFragment();
+            case 4:
+                return new ProfileFragment();
+            default:
+                return new HomeFragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 5;
+    }
+}
--- Final_Project/app/src/main/java/com/example/final_project/activity/SignupActivity.java
+++ Final_Project/app/src/main/java/com/example/final_project/activity/SignupActivity.java
@@ -14,67 +14,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.final_project.R;
 
 public class SignupActivity extends AppCompatActivity {
-    private EditText edtEmail, edtPassword, edtConfirmPassword;
-    private Button btnSignup;
 
-    //    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
-
-//        mAuth = FirebaseAuth.getInstance();
-        edtEmail = findViewById(R.id.edt_email);
-        edtPassword = findViewById(R.id.edt_password);
-        edtConfirmPassword = findViewById(R.id.edt_confirmPassword);
-        btnSignup = findViewById(R.id.btn_login);
-
-        btnSignup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                signup();
-            }
-        });
-}
-
-    private void signup() {
-        String email, password, confirm;
-        email = edtEmail.getText().toString();
-        password = edtPassword.getText().toString();
-        confirm = edtConfirmPassword.getText().toString();
-
-        if (TextUtils.isEmpty(email)) {
-            Toast.makeText(this, "Vui lòng nhập email !!!", Toast.LENGTH_LONG).show();
-            return;
-        }
-
-        if (TextUtils.isEmpty(password)) {
-            Toast.makeText(this, "Vui lòng nhập mật khẩu !!!", Toast.LENGTH_LONG).show();
-            return;
-        }
-
-        if (TextUtils.isEmpty(confirm)) {
-            Toast.makeText(this, "Vui lòng nhắc lại mật khẩu !!!", Toast.LENGTH_LONG).show();
-            return;
-        }
-
-        if(confirm != password) {
-            Toast.makeText(this, "Vui lòng nhắc lại đúng mật khẩu !!!", Toast.LENGTH_LONG).show();
-            return;
-        }
-//        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult> () {
-//            @Override
-//            public void onComplete(@NonNull @NonNull Task<AuthResult> task) {
-//                if(task.isSuccessful()) {
-//                    Toast.makeText(getApplicationContext(), "Tạo tài khoản thành công", Toast.LENGTH_LONG).show();
-//                    Intent intent = new Intent(SignupActivity.this, MainActivity.class);
-//                    startActivity(intent);
-//                }
-//
-//                else {
-//                    Toast.makeText(getApplicationContext(), "Tạo tài khoản thất bại", Toast.LENGTH_LONG).show();
-//                }
-//            }
-//        })
     }
+
 }
